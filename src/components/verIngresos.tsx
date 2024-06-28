c26cd1570b30a19c0e8497a57c89ad6c8ff4eec9
Thomazoide <ttellerias01@outlook.com>
2024-06-28 01:40:17 -0400

    filtrado de datos listo, falta ui de ingresos

---
+++ src/components/verIngresos.tsx
@@ -0,0 +1,10 @@
+import { ingreso } from "@/types/ingreso"
+import { useState, useEffect } from "react"
+
+type propsVI = {
+    ingresos: ingreso[]
+}
+
+export default function VerIngresos(props: Readonly<propsVI>){
+    
+}
