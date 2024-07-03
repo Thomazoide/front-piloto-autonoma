6c8090a6e1c607d199d701f54f16510bebe9fca8
Thomazoide <ttellerias01@outlook.com>
2024-07-03 01:47:07 -0400

    monitor guardias in progress

--- src/components/monitorGuardia.tsx
+++ src/components/monitorGuardia.tsx
@@ -1,10 +1,22 @@
+import { ingreso } from "@/types/ingreso";
 import { worker } from "@/types/worker";
+import { Button, ScrollShadow } from "@nextui-org/react";
 import axios, { AxiosResponse } from "axios";
-import { ReactElement, useState, useEffect } from "react";
+import { ReactElement, useState, useEffect, MouseEvent } from "react";
+import { getIngresoByWorker } from "./utils/function_lib";
 
 
 export default function MonitorGuardias(): ReactElement{
     const [guardias, setGuardias] = useState<worker[]>()
+    const [selectedGuardia, setSelectedGuardia] = useState<worker>()
+    const [ingresos, setIngresos] = useState<ingreso[]>()
+
+    const handleGuardSelect = async (e: MouseEvent<HTMLButtonElement>): Promise<void> => {
+        const guardia: worker = JSON.parse(e.currentTarget.value)
+        const ingresosGuardia: ingreso[] = (await axios.get(`http://52.201.181.178:3000/api/ingreso/guardia/${guardia.id}`)).data
+        setSelectedGuardia(guardia)
+        setIngresos(ingresosGuardia)
+    }
 
     useEffect( () => {
         if(!guardias){
@@ -14,8 +26,29 @@ export default function MonitorGuardias(): ReactElement{
     }, [guardias] )
 
     return(
-        <div className="">
-            
+        <div className="flex flex-wrap gap-4">
+            <div className="items-center flex flex-col">
+                <h5>Guardias</h5>
+                {guardias ? <ScrollShadow className=" max-w-[370px] h-[200px]" style={{overflowY: "scroll"}}>
+                    {
+                        guardias.map( (g: worker) => (
+                            <div>
+                                <Button className="m-[5px] " color="danger" variant="bordered" value={JSON.stringify(g)} onClick={handleGuardSelect}>
+                                    {g.nombre} | {g.rut}
+                                </Button>
+                            </div>
+                        ) )
+                    }
+                </ScrollShadow> : null}
+            </div>
+            {
+                selectedGuardia && ingresos && ingresos[0] ?
+                <div></div>
+                : selectedGuardia && ingresos && !ingresos[0] ?
+                <div></div>
+                : null
+
+            }
         </div>
     )
 }
