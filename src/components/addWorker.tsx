c8a93435501f7e3cc32f2d702815e68394aacd0e
Thomazoide <ttellerias01@outlook.com>
2024-07-05 01:47:13 -0400

    formulario guardia;

--- src/components/addWorker.tsx
+++ src/components/addWorker.tsx
@@ -2,6 +2,7 @@ import { Divider } from "@fluentui/react-components"
 import { Button } from "@nextui-org/button"
 import { Input } from "@nextui-org/input"
 import { ReactElement, useState } from "react"
+import { timeOut } from "./utils/function_lib"
 
 
 type AWProps = {
@@ -21,16 +22,21 @@ export default function AddWorker(props: Readonly<AWProps>): ReactElement{
     const crearBeacon = async function(): Promise<void>{
     }
 
+    const handleButton = (): void => {
+        setIsLoading(true)
+        timeOut( () => setIsLoading(false), 300 )
+    }
+
 
     return(
         <div className="flex flex-col gap-6 min-w-[300px] min-h-[300px] max-w-[500px] border-double border-2 border-red-300 rounded-lg p-[15px] ">
-            <Input type="text" label="Nombre" placeholder="nombre apellido"/>
-            <Input type="text" label="Rut" placeholder="sin puntos y con guion"/>
-            <Input type="email" label="Email" placeholder="ejemplo@mail.com"/>
-            <Input startContent={"+56"} type="text" label="Celular" placeholder="8 digitos"/> 
-            <Input type="text" label="MAC beacon" placeholder="Sin los puntos dobles y en mayusculas"/>
+            <Input type="text" label="Nombre" placeholder="nombre apellido" onValueChange={setNombre}/>
+            <Input type="text" label="Rut" placeholder="sin puntos y con guion" onValueChange={setRut}/>
+            <Input type="email" label="Email" placeholder="ejemplo@mail.com" onValueChange={setEmail}/>
+            <Input startContent={"+56"} type="text" label="Celular" placeholder="8 digitos" onValueChange={setCelular}/>
+            <Input type="text" label="MAC beacon" placeholder="Sin los puntos dobles y en mayusculas"onValueChange={setMac}/>
             <Divider />
-            <Button color="danger" variant="solid">
+            <Button color="danger" variant="solid" isLoading={isLoading} onClick={handleButton}>
                 Agregar guardia
             </Button>
         </div>
