49b5c89e725ac2972d4d68b3a06d64d445839dc5
Thomazoide <ttellerias01@outlook.com>
2024-07-29 15:15:38 -0400

    verificacion de token activo

--- src/context/loginContext.tsx
+++ src/context/loginContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, Dispatch, ReactElement, ReactNode, useEffect, useReducer } from "react"
+import axios from "axios"
 
 export enum actionTypes {
     LOGIN = 'LOGIN',
@@ -42,14 +43,37 @@ const authReducer = (state: State, action: Action): State => {
 
 export const AuthContext = createContext<{state: State; dispatch: Dispatch<Action>} | undefined>(undefined)
 
+interface verifyResponse{
+    isValid: boolean
+}
+
+interface verifyTokenRequest{
+    token: string
+}
+
+async function verificarToken(payload: verifyTokenRequest): Promise<boolean>{
+    const response: verifyResponse = (await axios.post(`${import.meta.env.VITE_API_URL}/auth/verify`, payload)).data
+    return response.isValid
+}
+
 export const AuthContextProvider = ({children}: Readonly<{children: ReactNode}>): ReactElement => {
     const [state, dispatch] = useReducer(authReducer, estadoInicial)
 
     useEffect(() => {
-        const userData = localStorage.getItem('userData')
+        const userData: string | null = localStorage.getItem('userData')
         if(userData){
-            dispatch({type: actionTypes.LOGIN, payload: JSON.parse(userData)})
-            console.log('SESION ACTIVA')
+            const auxData: User = JSON.parse(userData)
+            const request: verifyTokenRequest = {
+                token: auxData.token
+            }
+            verificarToken(request).then( (isValid) => {
+                if(isValid){
+                    dispatch({type: actionTypes.LOGIN, payload: auxData})
+                    console.log('SESION ACTIVA')
+                } else {
+                    localStorage.removeItem('userData')
+                }
+            } )
         }
     }, [])
 
