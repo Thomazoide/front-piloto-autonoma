8d6ac5ff018371fbed37248fb38302967006c13b
Thomazoide ubuntu <ttellerias01@outlook.com>
2024-10-08 18:51:26 -0300

    dashboard 75%

--- src/components/charts/dashboardCharts.tsx
+++ src/components/charts/dashboardCharts.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState, ReactElement } from 'react'
 import { BarChart, Bar, ResponsiveContainer, CartesianGrid, XAxis, YAxis, Rectangle, Legend, Tooltip } from 'recharts'
 import { MonthAndAttendanceChartData, SortAttendanceData, MONTHS, MONTHS_NAMES } from '@/components/utils/function_lib'
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
-import { Spinner, Dropdown, DropdownMenu, DropdownItem } from '@nextui-org/react'
+import { Spinner, Dropdown, DropdownMenu, DropdownItem, DropdownTrigger, Button } from '@nextui-org/react'
 import "@/styles/charts.css"
 
 export default function Dashboard(): ReactElement{
@@ -95,46 +95,73 @@ export default function Dashboard(): ReactElement{
             {
                 ingresosSorted &&
                 <div className="flex flex-col gap-3 items center">
-                    <div className="flex flex-row justify-between p-[15px] bg-ua-gray bg-opacity-25 w-full h-fit rounded-xl shadow-xl  ">
-                        <div className="flex justify-center text-center ">
-                            <p>
-                                <strong>
-                                    Asistencia de personal
-                                </strong>
-                                <br/>
-                            </p>
-                            {
-                                ingresosSorted.map( (i, index) => (
-                                    <p key={index+1}>
-                                        <strong>
-                                            {i.sede}
-                                        </strong>
-                                        <br/>
-                                        Numero de trabajadores con asistencia: {i.numberOfWorkers}
-                                    </p>
-                                ) )
-                            }
-                        </div>
-                        <div className="flex justify-center items-center w-fit text-center">
-                            <h1 className="italic font-semibold" >
-                                Asistencia de sedes en el mes de {MONTHS_NAMES[selectedMonth]}
+                    <div className="flex flex-column items-center gap-3 justify-between p-[15px] bg-default-200 w-full h-fit rounded-xl shadow-xl  ">
+                        <div className="flex flex-col items-center w-fit text-center">
+                            <h1 className="italic font-semibold max-w-[800px]" >
+                                Numero de trabajadores con asistencia por sede
                             </h1>
+                            <div className="flex flex-col items-center gap-1 p-[15px] ">
+                                <Dropdown title='Seleccionar mes' aria-label='Seleccionar mes' >
+                                    <DropdownTrigger>
+                                        <Button color="danger" variant='bordered'>
+                                            {MONTHS_NAMES[selectedMonth]}
+                                        </Button>
+                                    </DropdownTrigger>
+                                    <DropdownMenu
+                                    variant="bordered">
+                                        {
+                                            MONTHS.map( (month, index) => (
+                                                <DropdownItem color="danger" variant="flat" className="text-start" key={month} value={MONTHS_NAMES[index]}>
+                                                    {MONTHS_NAMES[index]}
+                                                </DropdownItem>
+                                            ) )
+                                        }
+                                    </DropdownMenu>
+                                </Dropdown>
+                                <p className="italic">
+                                    <small>
+                                        Mes seleccionado: {MONTHS_NAMES[selectedMonth]}
+                                    </small>
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex justify-center min-h-[200px] min-w-[800px]  ">
+                            <ResponsiveContainer width={"100%"} height={"100%"}>
+                                <BarChart
+                                data={ingresosSorted}>
+                                    <CartesianGrid strokeDasharray={"3 3"}/>
+                                    <Legend />
+                                    <Tooltip/>
+                                    <XAxis dataKey={"sede"}/>
+                                    <YAxis/>
+                                    <Bar dataKey={"numberOfWorkers"} fill="#8884d8" activeBar={
+                                        <Rectangle fill="gold" stroke="purple"/>
+                                    }/>
+                                </BarChart>
+                            </ResponsiveContainer>
                         </div>
                     </div>
-                    <div className="flex justify-center m-[15px] min-h-[200px] min-w-[800px] ">
-                        <ResponsiveContainer width={"100%"} height={"100%"} >
-                            <BarChart
-                            data={ingresosSorted}>
-                                <CartesianGrid strokeDasharray={"3 3"}/>
-                                <Legend/>
-                                <Tooltip/>
-                                <XAxis dataKey={"sede"} />
-                                <YAxis />
-                                <Bar dataKey={"attendances"} fill='#8884d8' activeBar={
-                                    <Rectangle fill="gold" stroke="purple"/>
-                                }/>
-                            </BarChart>
-                        </ResponsiveContainer>
+                    <div className="flex flex-col items-center gap-3 w-fit h-fit p-[15px] bg-default-200 rounded-xl shadow-xl ">
+                        <div className="flex justify-center">
+                            <h1 className="italic font-semibold" >
+                                Asistencias marcadas por sede
+                            </h1>
+                        </div>
+                        <div className="flex justify-center m-[15px] min-h-[200px] min-w-[800px] ">
+                            <ResponsiveContainer width={"100%"} height={"100%"} >
+                                <BarChart
+                                data={ingresosSorted}>
+                                    <CartesianGrid strokeDasharray={"3 3"}/>
+                                    <Legend/>
+                                    <Tooltip/>
+                                    <XAxis dataKey={"sede"} />
+                                    <YAxis />
+                                    <Bar dataKey={"attendances"} fill='#8884d8' activeBar={
+                                        <Rectangle fill="gold" stroke="purple"/>
+                                    }/>
+                                </BarChart>
+                            </ResponsiveContainer>
+                        </div>
                     </div>
                 </div>
             }
