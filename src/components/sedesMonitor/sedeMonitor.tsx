7c4f10224be567a67cd136af7a00327240d2ea66
Thomazoide <ttellerias01@outlook.com>
2024-10-15 23:21:30 -0300

    Modulo sedes 40%

--- src/components/sedesMonitor/sedeMonitor.tsx
+++ src/components/sedesMonitor/sedeMonitor.tsx
@@ -1,14 +1,15 @@
-import { useState, useEffect, ReactElement } from "react";
+import { useState, useEffect, ReactElement, ChangeEvent } from "react";
 import { Select, SelectItem, SelectSection, Spinner } from "@nextui-org/react"
 import { GetAllSedes } from "@/components/utils/function_lib"
-import { sede } from "@/types/sede";
+import { GeoJson, sede } from "@/types/sede";
 import { useAuthContext } from "@/hooks/useLoginContext";
+import MapComponent from "./mapComponent";
 
 export default function SedeMonitor(): ReactElement{
     const [sedes, setSedes] = useState<sede[]>()
     const [selectedSede, setSelectedSede] = useState<sede>()
     const {state} = useAuthContext()
-    const [selectedFloor, setSelectedFloor] = useState<number>()
+    const [selectedFloor, setSelectedFloor] = useState<GeoJson<number[][][]>>()
     const [isLoading, setIsLoading] = useState<boolean>(false)
 
     async function getSedes(){
@@ -17,6 +18,13 @@ export default function SedeMonitor(): ReactElement{
             setSedes(await GetAllSedes(state.user.token))
             setIsLoading(false)
         }
+    }
+
+    function handleSedeSelection(e: ChangeEvent<HTMLSelectElement>){
+        const id: number = Number(e.target.value)
+        const item: sede | undefined = sedes?.find( (sede) => sede.id === id )
+        setSelectedSede(item)
+        
     }
     
     useEffect( () => {
@@ -28,11 +36,11 @@ export default function SedeMonitor(): ReactElement{
             <div className="flex max-w-[800px] min-w-[380px] justify-center" >
                 {
                     sedes && !isLoading ?
-                    <Select color="danger" variant="underlined" size="lg" label="Seleccionar sede">
+                    <Select color="danger" variant="underlined" size="lg" label="Seleccionar sede" onChange={handleSedeSelection}>
                         <SelectSection>
                             {
                                 sedes.map( (sede, index) => (
-                                    <SelectItem key={index} value={index}>
+                                    <SelectItem key={sede.id} value={index}>
                                         {sede.nombre}
                                     </SelectItem>
                                 ) )
@@ -43,12 +51,30 @@ export default function SedeMonitor(): ReactElement{
                 }
             </div>
             { selectedSede &&
-                <div className="flex flex-col items-center min-w-[380px] max-w-[800px] min-h-[400px] bg-default-900 bg-opacity-25 rounded-xl border-double border-2 border-danger-300 shadow-xl shadow-danger-200 ">
-                    { selectedSede.plantas &&
-                        <div className="flex justify-end w-full">
-                        
-                        </div>
-                    }
+                <div className="flex flex-col gap-3 p-[15px] items-center sm:w-[380px] sm:h-[800px] lg: bg-default-300 bg-opacity-25 rounded-xl border-double border-2 border-danger-300 shadow-xl shadow-danger-200 ">
+                    <div className="flex justify-end w-full">
+                        <Select color="danger" variant="underlined" size="md" label="Seleccionar planta" defaultSelectedKeys={['-1']}>
+                            <SelectSection>
+                                <SelectItem key="-1" value="-1">
+                                    Vista general
+                                </SelectItem>
+                            </SelectSection>
+                            { selectedSede && selectedSede.plantas &&
+                                <SelectSection>
+                                {
+                                    selectedSede.plantas.map( (planta, index) => (
+                                        <SelectItem textValue={`Piso ${index+1}`} key={index+1} value={JSON.stringify(planta)}>
+                                            Piso: {index+1}
+                                        </SelectItem>
+                                    ) )
+                                }
+                                </SelectSection>
+                            }
+                        </Select>
+                    </div>
+                    <div className="flex justify-center w-full h-full" >
+                        <MapComponent planta={selectedFloor} sede={selectedSede}/>
+                    </div>
                 </div>
             }
         </div>
