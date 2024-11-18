c5eb0f14970680f835cc1517c331449dddfd1681
Thomazoide <ttellerias01@outlook.com>
2024-11-18 13:43:01 -0300

    new home page in progress

---
+++ src/components/home/home.tsx
@@ -0,0 +1,64 @@
+import { worker } from "@/types/worker";
+import { Switch } from "@nextui-org/switch";
+import { useState, useEffect, ReactElement } from "react";
+import Dashboard from "../charts/dashboardCharts";
+import { GetActiveRooms, GetActiveWorkers, GetAllDocentes } from "../utils/function_lib";
+import { useAuthContext } from "@/hooks/useLoginContext";
+import { ChartMultiple16Regular } from "@fluentui/react-icons";
+import HomeMap from "./map";
+
+export default function HomeComponent(): ReactElement {
+    const [docentes, setDocentes] = useState<worker[]>()
+    const [docentesActivos, setDocentesActivos] = useState<number>()
+    const [salasActivas, setSalasActivas] = useState<number>()
+    const [seeCharts, setSeeCharts] = useState<boolean>(false)
+    const { state } = useAuthContext()
+
+    const classNames1: string = "flex flex-col items-center max-h-[75px gap-1 rounded-xl p-[15px] border-2 border-solid border-danger-300 text-center "
+
+    useEffect( () => {
+        const refetchInterval = setInterval( () => {
+            GetActiveRooms( state.user!.token )
+                .then( (value) => setSalasActivas(value) )
+            GetAllDocentes( state.user!.token )
+                .then( (workers) => setDocentes(workers) )
+            GetActiveWorkers("docente", state.user!.token)
+                .then( ( workers ) => setDocentesActivos( workers.length ) )
+        }, 15000 )
+        return () => clearInterval(refetchInterval)
+    }, [] )
+
+    return (
+        <div className="flex flex-col w-full items-center gap-3 p-[15px] ">
+            <div className="flex flex-row justify-center gap-5 ">
+                <div className={classNames1}>
+                    <p>
+                        Docentes activos
+                        <br/>
+                        {docentesActivos} de {docentes?.length}
+                    </p>
+                </div>
+                <div className={classNames1}>
+                    <p>
+                        Salas activas
+                        <br/>
+                        {salasActivas}
+                    </p>
+                </div>
+                <div className={classNames1}>
+                    <p>
+                        Ver estadísticas
+                    </p>
+                    <Switch color="danger" thumbIcon={
+                        <ChartMultiple16Regular/>
+                    } isSelected={seeCharts} onValueChange={setSeeCharts}/>
+                </div>
+            </div>
+            {
+                !seeCharts ?
+                <HomeMap token={state.user!.token} workers={docentes!} />
+                : <Dashboard/>
+            }
+        </div>
+    )
+}
---
+++ src/components/home/map.tsx
@@ -0,0 +1,55 @@
+import { ReactElement, useEffect, useState } from "react";
+import { MapContainer, TileLayer, GeoJSON, Popup } from "react-leaflet";
+import ReactleafletDriftMarker from 'react-leaflet-drift-marker'
+import { GetAllSedes } from "../utils/function_lib";
+import { worker } from "@/types/worker";
+import { sede } from "@/types/sede";
+import { Spinner } from "@nextui-org/react";
+import { LatLngExpression } from "leaflet";
+
+interface mapProps{
+    token: string
+    workers: worker[]
+}
+
+export default function HomeMap(props: Readonly<mapProps>): ReactElement{
+
+    const [sedes, setSedes] = useState<sede[]>()
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [center, setCenter] = useState<LatLngExpression>()
+    
+    function getSedes(){
+        setIsLoading(true)
+        GetAllSedes(props.token).then( (res) => setSedes(res) )
+        setIsLoading(false)
+    }
+
+    
+
+    useEffect( () => {
+        !sedes && getSedes()
+        sedes && setCenter({
+            lat: sedes![0].ubicacion.features[0].geometry.coordinates[1],
+            lng: sedes![0].ubicacion.features[0].geometry.coordinates[0]
+        })
+
+    }, [sedes] )
+
+    return(
+        <div className="flex justify-center w-full h-[800px] border-3 border-solid rounded-xl  ">
+           {!isLoading && center && sedes ? <MapContainer center={center} zoom={20} style={{
+                height: "100%",
+                width: "100%"
+            }}>
+                <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'/>
+                {
+                    sedes.map( (sede) => (
+                        <GeoJSON data={sede.m2}/>
+                    ) )
+                }
+            </MapContainer> : isLoading && <Spinner color="danger" size="lg"/>}
+        </div>
+    )
+}
